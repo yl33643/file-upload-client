3d00002217e1215f0482414693c47731b295336f
Kevin Yuan <yuanlin@zeaho.com>
2017-09-11 14:31:07 +0800

    add upload limit

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,8 @@
 
 <template>
-  <el-upload action="http://localhost:3000/upload/single" name="single" :on-success="handleSuccess" :on-error="handleError" list-type="picture">
+  <el-upload action="http://localhost:3000/upload/single" name="single" :before-upload="beforeUpload" :on-success="handleSuccess" :on-error="handleError" list-type="picture">
     <el-button size="small" type="primary">点击上传</el-button>
+    <div slot="tip" class="el-upload__tip">只能上传jpg文件，且不超过2MB</div>
   </el-upload>
 </template>
 
@@ -9,6 +10,17 @@
 export default {
   name: 'hello',
   methods: {
+    beforeUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是JPG格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过2MB!');
+      }
+      return isJPG && isLt2M;
+    },
     handleSuccess() {
       this.$message.success('上传成功');
     },
